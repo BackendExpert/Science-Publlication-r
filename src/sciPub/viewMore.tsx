e7edcb0338c578619f30f9f5d21f28092d1c4e71
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 10:33:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/viewMore.tsx
+++ src/sciPub/viewMore.tsx
@@ -2,7 +2,8 @@
 const viewMore = () => {
   return (
     <div className="container mx-auto px-10">
-        Hi a;;
+        <p className="">Stop the next pandemic? Stop illegal wildlife trade!</p>
+        <p className="">The role of wildlife trade in transmitting emerging diseases</p>
     </div>
   )
 }
