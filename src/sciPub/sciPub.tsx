ac29946a3a5d9934c4070ecb354f251244bc9b61
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 10:17:01 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/sciPub.tsx
+++ src/sciPub/sciPub.tsx
@@ -25,29 +25,6 @@ const sciPub = () => {
                 </a>
             </div>
         </div>
-        <div className="md:grid grid-cols-3 gap-4 my-12">
-            <div className="md:my-0 mt-8 mb-12">
-                <a href="" className="">
-                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
-                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
-                </a>
-            </div>
-            <div className="md:my-0 my-12">
-                <a href="" className="">
-                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
-                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
-                </a>
-            </div>
-            <div className="md:my-0 my-12">
-                <a href="" className="">
-                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
-                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
-                </a>
-            </div>
-        </div>
     </div>
   )
 }
