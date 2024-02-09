aadb3c56fc120b945077dba5704de1e2ec141b46
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 10:49:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/viewMore.tsx
+++ src/sciPub/viewMore.tsx
@@ -15,7 +15,7 @@ const viewMore = () => {
         </div>
 
         <div className="">
-            <p className="">Introduction</p>
+            <p className="text-xl font-semibold py-8">Introduction</p>
             <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa rerum laboriosam perspiciatis id laborum suscipit explicabo recusandae quasi deleniti, maiores consequuntur laudantium atque voluptatem nam magnam quis itaque, numquam vitae!
             Beatae laudantium, praesentium vero facilis assumenda eum doloribus. Cupiditate, et soluta. Reprehenderit ad ea laboriosam et accusantium hic tempore! Cum consequatur aliquid iure asperiores nulla. Nemo ullam nam saepe tempore!
             Dolorem, laboriosam quibusdam? Officiis molestiae hic delectus ipsam, voluptates facere? Consequatur impedit fugit quaerat eaque. Esse dignissimos molestias minus minima tempora cumque et sint aliquam, repudiandae cum sapiente quaerat ab!
@@ -29,7 +29,7 @@ const viewMore = () => {
         </div>
 
         <div className="">
-            <p className="">What is the species spillover of pathogens ?</p>
+            <p className="text-xl font-semibold py-8">What is the species spillover of pathogens ?</p>
             <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque veritatis consectetur ea ab ex quae pariatur quas natus, veniam magni est officiis sequi perferendis recusandae quibusdam unde, accusantium nobis?
             Ex dolorum sit hic, facere error autem minima optio, numquam eos fugit, quasi sed amet? Eum voluptates possimus cupiditate provident iste laudantium harum in quidem excepturi ipsa! Itaque, dicta. Eos?
             Veniam doloremque vel odit aspernatur sequi sapiente praesentium, facere in perferendis reiciendis perspiciatis molestias debitis commodi unde eligendi ad est eum recusandae sint cum? Distinctio recusandae dolorem dignissimos corporis consequatur.
@@ -45,7 +45,7 @@ const viewMore = () => {
         </div>
 
         <div className="">
-            <p className="">Controversial origin fo COVID-19: From Bat to Pangolins to Humans ?</p>
+            <p className="text-xl font-semibold py-6">Controversial origin fo COVID-19: From Bat to Pangolins to Humans ?</p>
             <p className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae placeat nulla laboriosam. Praesentium, quasi facilis ex, adipisci minima atque doloremque at id aliquam quis odit illo molestiae, quae est dolores!
             Soluta aut, totam est blanditiis velit ex nulla adipisci esse eos aliquid quam natus fuga eveniet reprehenderit in ut eum, veniam veritatis repellendus saepe nihil ad molestiae! Id, similique recusandae.
             Rem nemo eveniet ea perferendis molestiae quibusdam, quod, architecto incidunt ex libero, atque quae corrupti id illo recusandae temporibus voluptatem quaerat eos nam sed consectetur mollitia. Eligendi eaque laborum error!
@@ -66,6 +66,20 @@ const viewMore = () => {
         </div>
         <div className="">
             <img src="https://images.wallpaperscraft.com/image/single/bmw_car_front_view_164813_300x188.jpg" alt="" className="rounded w-3/4 mx-40 h-3/6 my-8"/>
+        </div>
+
+        <div className="mb-16">
+            <p className="text-xl font-semibold py-6">Other pandemics of the century and wildlife trade</p>
+            <p className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam consequatur dignissimos repellendus ad aperiam, quaerat earum amet voluptatem quos, qui tempore dicta. Obcaecati atque illum eligendi ratione nesciunt ea mollitia!
+            Porro esse qui id, rerum ex iste voluptatem alias delectus excepturi, voluptates, totam voluptate ut harum ipsa nobis nam possimus? Accusamus natus eveniet atque labore quibusdam quaerat modi soluta culpa?
+            Distinctio vitae architecto harum assumenda illo sit. Fugit quae accusamus veniam nulla temporibus necessitatibus nisi. Deserunt nobis dolore consequatur itaque qui aliquam, reprehenderit consequuntur corporis exercitationem, odio autem nostrum pariatur!
+            Adipisci eum laborum voluptate dolorem voluptatum, sunt quae magnam laudantium enim in officia, officiis sint quisquam ipsum. Quaerat quia suscipit rerum beatae voluptas, nisi tempora sed facere doloribus, deserunt adipisci?
+            Explicabo alias error repellendus magnam quisquam maiores ut soluta eveniet. Maiores saepe neque accusantium exercitationem hic libero earum omnis aliquam, distinctio, quas dignissimos ratione debitis quia sit mollitia eaque repellendus?
+            Nihil, reprehenderit laborum porro facilis architecto nam debitis libero temporibus! Nostrum velit corporis nobis sit recusandae doloribus odio eveniet inventore similique, ea eius vel dolorem consequatur officia, deserunt id dolorum.
+            Facilis non sunt dolorum labore veritatis eum? Debitis omnis aliquid nisi cum? Sed, eaque unde debitis eius esse harum recusandae rem a sit veniam animi maiores earum ab deleniti aliquam!
+            Laborum quis in voluptatibus dignissimos itaque quaerat, explicabo rem perspiciatis aliquam reiciendis, mollitia excepturi corrupti non aperiam minima nemo. Dolor ad dolorem expedita necessitatibus tempore inventore? Consequuntur temporibus laudantium voluptates.
+            Debitis, itaque, fugiat repellat asperiores iure, officia obcaecati magni libero explicabo blanditiis reiciendis commodi. Quaerat tenetur, culpa dolor doloribus, saepe doloremque voluptate voluptatibus eligendi placeat quam voluptatum quas non officiis!
+            Vero et ab quae veniam ipsa eum vel aperiam fugit quibusdam nemo ad aspernatur, eaque delectus eius amet ipsum quasi dolorem voluptate iure dolorum aut quia fugiat. Sed, rerum minima.</p>
         </div>
     </div>
   )
