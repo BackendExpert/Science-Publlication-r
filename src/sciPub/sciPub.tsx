ba2406ec408003f6a61ace102086b561f8c2bf70
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 09:43:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/sciPub.tsx
+++ src/sciPub/sciPub.tsx
@@ -4,7 +4,7 @@ const sciPub = () => {
         <h1 className="">Recent Scientific Publication</h1>
         <div className="md:grid grid-cols-3 gap-4">
             <div className="">
-                <img src="https://images.wallpaperscraft.com/image/single/bmw_m3_sports_94739_300x168.jpg" alt="" className="rounded" />
+                <img src="https://images.wallpaperscraft.com/image/single/bmw_m3_sports_94739_300x168.jpg" alt="" className="rounded w-full" />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem officiis quo, maiores saepe est, mollitia ea molestias repellendus id repellat deleniti amet sunt, animi architecto quidem sapiente facilis tenetur!</p>
             </div>
             <div className="">
