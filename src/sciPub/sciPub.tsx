ce5fd7cf5442196cc98fae0ddc18de8c9cca642f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 10:29:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/sciPub.tsx
+++ src/sciPub/sciPub.tsx
@@ -13,18 +13,18 @@ const sciPub = () => {
                 </Link>
             </div>
             <div className="md:my-0 my-12">
-                <a href="" className="">
+                <Link to={'/scipub/viewMore'}>
                     <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
                     <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
                     <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
-                </a>
+                </Link>
             </div>
             <div className="md:my-0 my-12">
-                <a href="" className="">
+                <Link to={'/scipub/viewMore'}>
                     <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
                     <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
                     <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
-                </a>
+                </Link>
             </div>
         </div>
     </div>
