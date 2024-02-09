c7c5b869f47bed6b597a11813cb46dde43fabf29
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 10:15:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/sciPub.tsx
+++ src/sciPub/sciPub.tsx
@@ -4,36 +4,48 @@ const sciPub = () => {
         <h1 className="text-center my-8 text-3xl font-semibold">Recent Scientific Publication</h1>
         <div className="md:grid grid-cols-3 gap-4 mt-4 mb-12">
             <div className="md:my-0 mt-8 mb-12">
-                <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
-                <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
+                <a href="" className="">
+                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
+                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
+                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
+                </a>
             </div>
             <div className="md:my-0 my-12">
-                <img src="https://images.wallpaperscraft.com/image/single/bmw_m3_sports_94739_300x168.jpg" alt="" className="rounded w-full" />
-                <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                <p className="mt-8 text-md font-300">The Science of Aji-no-moto</p>
+                <a href="" className="">
+                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
+                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
+                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
+                </a>
             </div>
             <div className="md:my-0 my-12">
-                <img src="https://images.wallpaperscraft.com/image/single/bmw_m3_sports_94739_300x168.jpg" alt="" className="rounded w-full" />
-                <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                <p className="mt-8 text-md font-300">The present Covid-19</p>
+                <a href="" className="">
+                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
+                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
+                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
+                </a>
             </div>
         </div>
         <div className="md:grid grid-cols-3 gap-4 my-12">
             <div className="md:my-0 mt-8 mb-12">
-                <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
-                <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
+                <a href="" className="">
+                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
+                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
+                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
+                </a>
             </div>
             <div className="md:my-0 my-12">
-                <img src="https://images.wallpaperscraft.com/image/single/bmw_m3_sports_94739_300x168.jpg" alt="" className="rounded w-full" />
-                <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                <p className="mt-8 text-md font-300">The Science of Aji-no-moto</p>
+                <a href="" className="">
+                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
+                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
+                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
+                </a>
             </div>
             <div className="md:my-0 my-12">
-                <img src="https://images.wallpaperscraft.com/image/single/bmw_m3_sports_94739_300x168.jpg" alt="" className="rounded w-full" />
-                <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
-                <p className="mt-8 text-md font-300">The present Covid-19</p>
+                <a href="" className="">
+                    <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
+                    <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
+                    <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
+                </a>
             </div>
         </div>
     </div>
