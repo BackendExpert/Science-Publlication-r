e4855f181722b823ba979ca3296b74036de77feb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 09:56:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/sciPub.tsx
+++ src/sciPub/sciPub.tsx
@@ -1,7 +1,7 @@
 const sciPub = () => {
   return (
     <div className="container mx-auto px-10">
-        <h1 className="">Recent Scientific Publication</h1>
+        <h1 className="text-center my-8 text-3xl font-semibold">Recent Scientific Publication</h1>
         <div className="md:grid grid-cols-3 gap-4 mt-4 mb-12">
             <div className="md:my-0 mt-8 mb-12">
                 <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
