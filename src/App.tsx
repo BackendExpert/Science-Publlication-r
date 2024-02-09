6681f9d6295793470dbf9a246beca16f955d3c35
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 10:22:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
 import SciPub from "./sciPub/sciPub"
 
 export default function App() {
   return (
-    <SciPub />
+    // <SciPub />
+
+    <BrowserRouter>
+      
+    </BrowserRouter>
 
   )
 }
