d59bebd4d57313fd28843da616f3c8ffff5965c2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 10:19:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/sciPub.tsx
+++ src/sciPub/sciPub.tsx
@@ -1,10 +1,14 @@
+import ViewMore from "./viewMore"
+
+
+
 const sciPub = () => {
   return (
     <div className="container mx-auto px-10">
         <h1 className="text-center my-8 text-3xl font-semibold">Recent Scientific Publication</h1>
         <div className="md:grid grid-cols-3 gap-4 mt-4 mb-12">
             <div className="md:my-0 mt-8 mb-12">
-                <a href="" className="">
+                <a href={<ViewMore />} className="">
                     <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-screen" />
                     <p className="mt-[-40px] ml-8 text-white font-semibold">Awareness</p>
                     <p className="mt-8 text-md font-300">Stop the next pandemic? Stop illegal wildlife trade!</p>
