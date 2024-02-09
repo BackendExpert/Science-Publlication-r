172c71ddeb770480118dd469e1b9337f240d6829
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 10:39:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/viewMore.tsx
+++ src/sciPub/viewMore.tsx
@@ -6,12 +6,42 @@ const viewMore = () => {
         <p className="">The role of wildlife trade in transmitting emerging diseases</p>
         <div className="md:grid grid-cols-2 gap-4">
             <div className="">
-                <img src="https://images.wallpaperscraft.com/image/single/car_red_sports_car_142598_300x168.jpg" alt="" className="rounded w-full"/>
+                <img src="https://images.wallpaperscraft.com/image/single/bmw_car_bumper_191131_300x188.jpg" alt="" className="rounded w-full"/>
             </div>
             <div className="">
                 <p className="">Dr. Shalini Rajakaruna</p>
                 <p className="">Published on Mon, 06/01/2020</p>
             </div>
+        </div>
+
+        <div className="">
+            <p className="">Introduction</p>
+            <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa rerum laboriosam perspiciatis id laborum suscipit explicabo recusandae quasi deleniti, maiores consequuntur laudantium atque voluptatem nam magnam quis itaque, numquam vitae!
+            Beatae laudantium, praesentium vero facilis assumenda eum doloribus. Cupiditate, et soluta. Reprehenderit ad ea laboriosam et accusantium hic tempore! Cum consequatur aliquid iure asperiores nulla. Nemo ullam nam saepe tempore!
+            Dolorem, laboriosam quibusdam? Officiis molestiae hic delectus ipsam, voluptates facere? Consequatur impedit fugit quaerat eaque. Esse dignissimos molestias minus minima tempora cumque et sint aliquam, repudiandae cum sapiente quaerat ab!
+            Aliquid illo, magni dolore rerum modi, officiis culpa, voluptate fuga tempore ducimus cumque quis omnis eveniet at quae illum saepe. Cum cumque, quos quas atque quam beatae! Nisi, modi voluptate?
+            Fugit blanditiis quod autem atque ipsum laudantium suscipit doloribus, voluptatibus vero excepturi? Qui, nobis animi ducimus accusantium inventore esse. Quibusdam sunt repudiandae ducimus reiciendis sequi unde enim blanditiis ratione voluptatibus.
+            Rerum eligendi maxime in error neque nemo necessitatibus blanditiis reprehenderit nobis sed molestias, nostrum minima ducimus nulla odit quisquam fugiat laboriosam qui tenetur vero rem, non, culpa quod accusamus. Ratione.
+            Nesciunt velit eius dolorum temporibus nisi sint placeat aliquam architecto consequuntur, cum odio eos eveniet! Alias quas earum nam minus atque porro consectetur ratione, tempora distinctio incidunt rem neque nihil?
+            Incidunt aspernatur consectetur suscipit nesciunt nostrum voluptatibus dignissimos cum veniam doloribus ea. Porro, fugiat alias. Labore, laborum. Exercitationem excepturi obcaecati vero quasi, magnam amet aliquid, eius ipsum molestiae doloremque voluptates?
+            Eos quia animi dolore. Distinctio expedita aliquam enim nulla est animi earum veniam! Qui, amet veritatis. Voluptatibus perspiciatis beatae laboriosam natus ab illum eveniet ratione omnis itaque? Voluptatibus, repudiandae odio.
+            Optio ut illo esse veritatis quis! Iure fuga eaque neque ipsam, minus non consectetur architecto eos officia consequatur eius, dignissimos similique. Similique, molestias asperiores culpa quibusdam ipsa laborum perspiciatis magnam!</p>
+        </div>
+
+        <div className="">
+            <p className="">What is the species spillover of pathogens ?</p>
+            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloremque veritatis consectetur ea ab ex quae pariatur quas natus, veniam magni est officiis sequi perferendis recusandae quibusdam unde, accusantium nobis?
+            Ex dolorum sit hic, facere error autem minima optio, numquam eos fugit, quasi sed amet? Eum voluptates possimus cupiditate provident iste laudantium harum in quidem excepturi ipsa! Itaque, dicta. Eos?
+            Veniam doloremque vel odit aspernatur sequi sapiente praesentium, facere in perferendis reiciendis perspiciatis molestias debitis commodi unde eligendi ad est eum recusandae sint cum? Distinctio recusandae dolorem dignissimos corporis consequatur.
+            Voluptates incidunt ipsa quibusdam, est ut aliquam, dicta deserunt at animi aliquid aspernatur id earum fuga et tempore vel quas suscipit rerum soluta praesentium eveniet! Vero, architecto. Tempora, saepe rerum?
+            Qui, ab cupiditate consequuntur culpa nobis assumenda quisquam ipsa enim quos nihil sunt harum voluptate eius animi voluptatum deserunt neque minus obcaecati in esse. Aspernatur doloribus voluptatem praesentium quod atque.
+            Doloremque aut error, quis quibusdam tempora consectetur minus odit temporibus maxime reprehenderit cum, quae iste atque placeat nulla et sequi quod deleniti accusamus voluptatem aliquid ipsa quas exercitationem. Dolores, voluptas!
+            Vero, accusantium, dolore explicabo perspiciatis illum suscipit, corporis enim vel repudiandae adipisci consequatur sint accusamus maiores nostrum! Ipsa, autem animi aliquid minus inventore, sed in quo quam libero a aperiam.
+            Quo architecto provident quos voluptatum ad facere dicta animi itaque corrupti? Officia maiores corporis labore veritatis alias iste odio sunt tenetur vitae ad ea, praesentium repudiandae quaerat eveniet ab adipisci.</p>
+        </div>
+
+        <div className="">
+            <img src="https://images.wallpaperscraft.com/image/single/nissan_gtr_supercar_121502_300x188.jpg" alt="" />
         </div>
     </div>
   )
