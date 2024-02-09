aa1f0582698f7406f3cf74627052fd90047caf1d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 09:41:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/sciPub/sciPub.tsx
+++ src/sciPub/sciPub.tsx
@@ -1,6 +1,13 @@
 const sciPub = () => {
   return (
-    <div>sciPub</div>
+    <div className="container mx-auto px-10">
+        <h1 className="">Recent Scientific Publication</h1>
+        <div className="md:grid grid-3 gap-4">
+            <div className="">
+                <img src="" alt="" className="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
