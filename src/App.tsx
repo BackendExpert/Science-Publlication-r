b6b1b168cb57cb003915bb49a11d704767399836
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 09:39:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import SciPub from "./sciPub/sciPub"
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <SciPub />
   )
 }
